c62e52cff14b1734dd3e3695f8d52814ab275b32
Youngwoo Kim <52284829+kouym7979@users.noreply.github.com>
2021-01-30 18:37:03 +0900

    [update] list_design
    feat. 도서목록xml update, 파이어베이스 연동

--- app/src/main/java/com/example/openapi/Adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/openapi/Adapter/RecyclerViewAdapter.kt
@@ -1,29 +1,29 @@
 package com.example.openapi.Adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.openapi.MainActivity
 import com.example.openapi.R
-import com.example.openapi.ResponseDTO
 import kotlinx.android.synthetic.main.list_item.view.*
 
 class RecyclerViewAdapter(val bookList : MainActivity.ResponseDTO):RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.ViewHolder {
-       val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
-        return ViewHolder(view)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindItems(bookList.row[position])
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
         return bookList.row.count()
     }
-    
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItems(bookList.row[position])
+
+    }
     class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
 
         fun bindItems(data : MainActivity.Book){
--- app/src/main/java/com/example/openapi/MainActivity.kt
+++ app/src/main/java/com/example/openapi/MainActivity.kt
@@ -3,32 +3,40 @@ package com.example.openapi
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.openapi.Adapter.RecyclerViewAdapter
 import com.example.openapi.databinding.ActivityMainBinding
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
+import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
-import org.json.JSONArray
-import org.json.JSONObject
 import java.io.IOException
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
-
+    //private var main : RecyclerView ?=null
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
 
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         binding.mainList.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
         binding.mainList.setHasFixedSize(true)
+
+
+
         //binding.mainList.adapter = RecyclerViewAdapter(ResponseDTO(0,List(Place)))
+
+        /*main?.layoutManager =LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        main_list.setHasFixedSize(true)*/
         fetchJson()
     }
 
@@ -37,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         //val url ="http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=528dJYSZAL4MMDEHve563fHM6WAvppwdZfW1xc15tszh9OIoZE7HTpLIObCxY%2BrWUIRL0O9SQiMqKp6GG6vA9Q%3D%3D&numOfRows=10&pageNo=1"
         //val url ="http://data.seoul.go.kr/dataList/datasetView.do?infId=OA-149&srvType=A&serviceKind=1&currentPageNo=1"
         val key ="756f7243706b6f7538305269594e44"
-        val url="http://openapi.seoul.go.kr:8088/"+key+"/json/SeoulLibraryBookRentNumInfo/1/15/"
+        val url="http://openapi.seoul.go.kr:8088/"+key+"/json/SeoulLibraryBookRentNumInfo/1/100/"
         val request = Request.Builder().url(url).build()
 
         val client = OkHttpClient()
@@ -62,7 +70,9 @@ class MainActivity : AppCompatActivity() {
                 Log.d("확인",books.row[1].TITLE)
 
                 runOnUiThread {
-                    binding.mainList.adapter = RecyclerViewAdapter(books)
+                    Log.d("확인","ui 리싸이클러뷰 작동중입니다.")
+                    main_list.adapter = RecyclerViewAdapter(books)
+
                 }
 
             }
@@ -71,6 +81,8 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+
     data class ResponseDTO(val list_total_count:Int, val row: List<Book>)
     data class Book(
         val TITLE: String,
